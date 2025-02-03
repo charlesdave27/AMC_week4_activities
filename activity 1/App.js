7173976c59b43e395abccadce1227f4d605ba9ca
charlesdave27 <godseeker027@gmail.com>
2025-02-03 12:47:23 +0800

    Flatlist

---
+++ activity 1/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {ScrollView, Text,} from 'react-native';
+
+
+const App = () => (
+  <ScrollView>
+    <Text style = {{
+      color: 'red',
+    }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </Text>
+  </ScrollView>
+);
+
+export default App;
